b00b17d71192865eacab33f6ab1e3d9c3da60b6d
inrage <70843287+inrag3@users.noreply.github.com>
2023-02-10 21:15:45 +0300

    Bring code to PEP8

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Post, Group
 
+
 class PostAdmin(admin.ModelAdmin):
     list_display = (
         'pk',
@@ -14,6 +15,7 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
 
+
 class GroupAdmin(admin.ModelAdmin):
     list_display = (
         'pk',
@@ -25,5 +27,6 @@ class GroupAdmin(admin.ModelAdmin):
     list_filter = ('title',)
     empty_value_display = '-пусто-'
 
-admin.site.register(Post, PostAdmin) 
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Group, GroupAdmin)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Post, Group
 
+
 def index(request):
     template = 'posts/index.html'
     posts = Post.objects.all()[:10]
