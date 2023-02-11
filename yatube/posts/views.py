d9969b8853afafe94b865265c94525e999a70ed5
inrage <70843287+inrag3@users.noreply.github.com>
2023-02-11 14:04:23 +0300

    Fix bugs with Group model

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -15,7 +15,8 @@ def index(request):
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    # Теперь стало более логично брать посты у группы, через related_name :)
+    posts = group.posts.order_by('-pub_date')[:10]
     context = {
         'group': group,
         'posts': posts,
