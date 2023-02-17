a5fa43e8c51f4b88ee617f7d6d09f5bb6a8fc913
inrage <70843287+inrag3@users.noreply.github.com>
2023-02-18 01:07:38 +0300

    Add profile page

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Group
+from .models import Post, Group, User
 
 
 def index(request):
@@ -35,17 +35,27 @@ def group_posts(request, slug):
 
 
 def profile(request, username):
-    context = {
+    template = 'posts/profile.html'
+    user = User.objects.get(username=username)
+    post_list = user.posts.all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
+    context = {
+        'user': user,
+        'page_obj': page_obj,
+        'count': post_list.count(),
     }
-    return render(request, 'posts/profile.html', context)
+    return render(request, template, context)
 
 
 def post_detail(request, post_id):
+    template = 'posts/post_detail.html'
     post = Post.objects.get(pk=post_id)
     count = Post.objects.all().filter(author=post.author).count()
     context = {
         'post': post,
         'count': count,
     }
-    return render(request, 'posts/post_detail.html', context)
+    return render(request, template, context)
