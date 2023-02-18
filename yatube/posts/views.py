60c09c61d7fd7a43ab06828f1aba186b8b9fd82f
inrage <70843287+inrag3@users.noreply.github.com>
2023-02-18 22:33:35 +0300

    Add opportunity to edit posts

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -21,7 +21,7 @@ def index(request):
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    # Наверно стоит вынести повторяющиеся код
+
     post_list = group.posts.all()
     paginator = Paginator(post_list, 10)
     page_number = request.GET.get('page')
@@ -74,3 +74,26 @@ def post_create(request):
             return redirect(f'/profile/{post.author}/', {'form': form})
     form = PostForm()
     return render(request, template, {'form': form})
+
+
+@login_required
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    is_edit = True
+    template = 'posts/create_post.html'
+    if request.user == post.author:
+        if request.method == 'POST':
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                post.text = form.cleaned_data['text']
+                post.group = form.cleaned_data['group']
+                post.save()
+                return redirect('posts:post_detail', post.id)
+        form = PostForm(instance=post)
+        context = {
+            'form': form,
+            'is_edit': is_edit,
+            'post': post,
+        }
+        return render(request, template, context)
+    return redirect("posts:post_detail", post_id)
